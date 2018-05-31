27e1f00a852733e5f3c84b7bf006f7eac6691a49
BukirevaLiudmila <226585@niuitmo.ru>
2018-05-31 09:41:16 +0300

    completed TodoList app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Header = ({removeTodo}) => (
-    <div className="header">
-        <h1>TODO list</h1>
-        <div className="trash" onClick={() => removeTodo()}/>
-    </div>
+	<div className="header">
+		<h1>TODO list</h1>
+		<div className="trash" onClick={() => removeTodo()}/>
+	</div>
 )
 
 export default Header;
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,38 +1,68 @@
 import React from 'react';
+import {findDOMNode} from 'react-dom'
+import ReactTooltip from 'react-tooltip'
 import generateId from "../helpers/generateId";
 
+
 class Input extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-        this.addNewTodo = this.addNewTodo.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {value: ''};
+		this.addNewTodo = this.addNewTodo.bind(this);
+		this.onChangeInput = this.onChangeInput.bind(this);
+	}
 
-    addNewTodo() {
-        this.props.addTodo({
-            id: generateId(),
-            text: this.state.value,
-            completed: false
-        })
-    }
+	addNewTodo() {
+		if (this.checkValue(this.state.value)) {
+			this.props.addTodo({
+				id: generateId(),
+				text: this.state.value,
+				completed: false
+			})
+		}
+	}
 
-    render() {
-        return (
-            <div className="footer">
-                <div
-                    className="plus"
-                    onClick={this.addNewTodo}
-                />
-                <input
-                    type="text"
-                    className="form-control "
-                    value={this.state.value}
-                    onChange={(event) => this.setState({value: event.target.value})}
-                    onKeyPress={(event) => {if (event.charCode === 13) this.addNewTodo()} }
-                />
-            </div>
-        )
-    }
+	checkValue(value) {
+		return (value && value.trim() !== '' && value.trim().length >= 3);
+	}
+
+	onChangeInput(event) {
+		const {target: {value}} = event;
+		this.setState({value})
+		if (this.checkValue(value) || !value) {
+			ReactTooltip.hide(findDOMNode(this.refs.inputTooltip));
+		}
+		else {
+			ReactTooltip.show(findDOMNode(this.refs.inputTooltip));
+		}
+	}
+
+	render() {
+		return (
+			<div className="footer" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+				<ReactTooltip
+					type="error"
+					effect="solid"
+				/>
+				<div
+					className="plus"
+					onClick={this.addNewTodo}
+				/>
+				<input
+					type="text"
+					className="form-control"
+					value={this.state.value}
+					data-tip="Некорректный ввод данных"
+					ref="inputTooltip"
+					data-event="customEvent"
+					onChange={this.onChangeInput}
+					onKeyPress={(event) => {
+						if (event.charCode === 13) this.addNewTodo()
+					}}
+				/>
+			</div>
+		)
+	}
 }
 
 export default Input;
